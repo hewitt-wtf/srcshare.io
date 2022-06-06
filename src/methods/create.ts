0f45face82ffee3debd4939c2239930fcf1e9b76
Mech <83923848+7heMech@users.noreply.github.com>
2022-06-06 11:45:05 +0300

    Restructured create.ts and removed .idea folder

--- src/methods/create.ts
+++ src/methods/create.ts
@@ -1,40 +1,51 @@
-import fetch from "node-fetch";
-import { addParam } from "../utils/addParam";
-import languages from "../utils/languages";
+import fetch from 'node-fetch'
+import { addParam } from '../utils/addParam'
+import languages from '../utils/languages'
 
-type language = typeof languages[number]; // Create a union string literal type from the array.
+type language = typeof languages[number] // Create a union string literal type from the array.
 
 type Code = {
-    code: string;
-    error?: string;
-    language?: language;
-    title?: string;
-    description?: string;
-};
+    code: string
+    error?: string
+    language?: language
+    title?: string
+    description?: string
+}
+
+let uri = 'https://api.srcshare.io/code'
 
 export default async (file: Code) => {
-    let uri = "https://api.srcshare.io/code";
-    if (!file) throw new Error("You have to pass in an object!");
+    handleParams(file)
+
+    let res = await sendRequest(file)
+
+    const id = await res.json()
+    return { url: `https://srcshare.io/?id=${id}`, id }
+}
+
+function handleParams (file: Code) {
+    if (!file) throw new Error('You have to pass in an object!')
     if (!file.code)
-        throw new Error("You need to give some code to upload first!");
+        throw new Error('You need to give some code to upload first!')
 
-    if (file.language) uri = addParam(uri, "language", file.language);
+    for (const param in file) {
+        // Ignoring code and error since they are sent in the body of the request.
+        if (param === 'code' || param === 'error') continue
+        if (file.hasOwnProperty(param)) {
+            addParam(uri, param, file[param])
+        }
+    }
+}
 
-    if (file.title) uri = addParam(uri, "title", file.title);
-
-    if (file.description) uri = addParam(uri, "description", file.description);
-
-    let res = await fetch(uri, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+async function sendRequest (file: Code) {
+    return await fetch(uri, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
             code: file.code,
-            error: file.error,
-        }),
-    }).catch((err) => {
-        throw new Error(err.message);
-    });
-
-    const id = await res.json();
-    return { url: `https://srcshare.io/?id=${id}`, id };
-};
+            error: file.error
+        })
+    }).catch(err => {
+        throw new Error(err.message)
+    })
+}
